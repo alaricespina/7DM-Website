73bd4704d5e46441576aafddbb7c33d46e2a3f9e
alaricespina <alaricsespina@gmail.com>
2020-11-27 13:18:36 +0800

    Hatdog

--- home/index.js
+++ home/index.js
@@ -1,13 +1,3 @@
-function changingPics() {
-    document.getElementById("Background").src = "1.jpg";
-}
-var pics = [];
-var no = 5;
-var i = 1;
-while (i<no+1){
-    var stri = i.toString();
-    var ext = ".jpg"
-    var name = stri.concat(ext)
-    pics.push(name)
-    i++
-}
+
+
+
